fbd8d5b3f92a7ea6ce05cc0d584e318518148189
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 20:15:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,4 +41,6 @@ def monopoly_with_second_tier
     def monopoly_with_fourth_tier
       pieces: 4,
       names: {
-        
+        :reading_railroad => {
+          "mortgage_value" => "$100"
+          
